1843c8f98452f9740378266d51f8e7bac3b99a1d
ReaperDev <arhamjain1607@gmail.com>
2026-07-27 02:36:34 +0530

    Time: 0 ms (100%), Space: 20 MB (66.85%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int i=0,j=1,mprofit=0;
+        while(i<j && j<prices.size()){
+            if(prices[i]>prices[j]){
+                i=j;
+                j=i+1;
+            }
+            else{
+                int diff=prices[j]-prices[i];
+                mprofit +=diff;
+                i=j;
+                j=i+1;
+            }
+        }
+        return mprofit;
+        
+    }
+};
