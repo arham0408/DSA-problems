c86fbbe31227c777208984982c4aa8514f382c6a
ReaperDev <arhamjain1607@gmail.com>
2026-08-02 20:59:29 +0530

    Time: 0 ms (100%), Space: 20.6 MB (52.99%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int count = 0;
+        int maxReach = 0;
+        int currEnd = 0;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            maxReach = max(maxReach, i + nums[i]);
+            if (i == currEnd) {
+                count++;
+                currEnd = maxReach;
+            }
+        }
+        return count;
+    }
+};
