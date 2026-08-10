c5cdd675cf3cfae0ce2e961b1536907e31012130
ReaperDev <arhamjain1607@gmail.com>
2026-08-10 11:18:16 +0530

    Time: 0 ms (100%), Space: 11.7 MB (73.21%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int longest = 0;
+        stack<int> st;
+        st.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                st.push(i);
+            }
+            else {
+                st.pop();
+                if (st.empty()) {
+                    st.push(i);
+                }
+                else {
+                    int len = i - st.top();
+                    longest = max(len, longest);
+                }
+            }
+        }
+        return longest;
+    }
+};
